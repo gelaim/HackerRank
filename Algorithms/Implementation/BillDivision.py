85436d5b09c9df3a9d1b0d5a6cbfe871777ddea8
Thiago <t.gelaim@gmail.com>
2021-09-07 09:47:56 -0300

    BillDivision.

---
+++ Algorithms/Implementation/BillDivision.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    total = sum(bill)
+    e_total = total - bill[k]
+    if e_total/2 == b:
+        print('Bon Appetit')
+    else:
+        print(int(b - e_total/2))
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
