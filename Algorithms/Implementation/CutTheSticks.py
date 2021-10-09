0bfd8722c9228d0b7b8ca1049e5684814e6efd99
Thiago <t.gelaim@gmail.com>
2021-10-09 10:54:25 -0300

    CutTheSticks.py

---
+++ Algorithms/Implementation/CutTheSticks.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    # Write your code here
+    x = sorted(arr)
+    result = []
+    to_cut = 0
+    while len(x)>0:
+        result.append(len(x))
+        to_cut = x[0]
+        while len(x)>0 and to_cut ==x[0]:
+            x.pop(0)
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
