043f9d6708dc4cdfd146118ba60130d5dee57435
Thiago <t.gelaim@gmail.com>
2022-01-04 11:15:16 -0300

    Greedy/MinimumAbsoluteDifferenceInAnArray.py

---
+++ Algorithms/Greedy/MinimumAbsoluteDifferenceInAnArray.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    # Write your code here
+    arr.sort()
+    
+    result = abs(arr[0]-arr[1])
+    for i in range(1, len(arr)):
+        result = min(abs(arr[i]-arr[i-1]),result)
+
+
+        
+
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
