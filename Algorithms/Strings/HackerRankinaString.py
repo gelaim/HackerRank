1aaee501bc3eac29e8d84185ff0dc18b4b4cfbdf
Thiago <t.gelaim@gmail.com>
2022-01-05 18:49:06 -0300

    HackerRankinaString.py

---
+++ Algorithms/Strings/HackerRankinaString.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    # Write your code here
+    pattern = 'hackerrank'
+    cont = 0
+    for i in range(len(s)):
+        if s[i] == pattern[cont]:
+            cont+=1
+        if cont == len(pattern):
+            return 'YES'
+    return 'NO'
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
